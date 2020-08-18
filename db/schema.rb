58a2940f1e229d3ceb71b221be03d429b21c6aa4
Eugenie Avedikian <eugenieavedikian@gmail.com>
2020-08-18 11:27:26 +0200

    adding validation on model location

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_134919) do
-
+ActiveRecord::Schema.define(version: 2020_08_17_145008) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +30,17 @@ ActiveRecord::Schema.define(version: 2020_08_17_134919) do
     t.index ["user_id"], name: "index_hairs_on_user_id"
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.bigint "hair_id", null: false
+    t.bigint "user_id", null: false
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["hair_id"], name: "index_locations_on_hair_id"
+    t.index ["user_id"], name: "index_locations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -47,4 +57,6 @@ ActiveRecord::Schema.define(version: 2020_08_17_134919) do
   end
 
   add_foreign_key "hairs", "users"
+  add_foreign_key "locations", "hairs"
+  add_foreign_key "locations", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Hair.destroy_all
+User.destroy_all
+User1 = { name: "Chewbacca", address: "48 allée du Blaster KASHYYYK", phone: "01 81 18 89 98", email: "cheewy@gmail.com", password: "123456" }
+User2 = { name: "Kal Drogo", address: "21 impasse du sud ESSOS", phone: "01 90 76 10 98",  email: "iloveKhalheesi@gmail.com", password: "123456" }
+User3 = { name: "Sulli", address: "104 place Louis Armand MONSTROPOLIS", phone: "01 23 66 90 45", email: "jesuisgentil@gmail.com", password: "123456" }
+User4 = { name: "Robinson Cruzoe", address: "Ile paumé dans le Pacifique", phone: "01 81 18 89 98", email: "clamerde@wanadoo.com", password: "123456" }
+User5 =  { name: "Tormund Giantsbane", address: "Beyond the Wall", phone: "01 81 18 89 98", email: "french", password: "123456" }
+[ User1, User2, User3, User4 ].each do |attributes|
+  user = User.create!(attributes)
+  puts "Created #{user.name}"
+end
+
+hair1 = Hair.create!(user: User.first)
+location1 = Location.create!(user: User.last, hair: hair1, start_date: Date.today, end_date: Date.tomorrow)
