f473d435c26c898454ac76691a23c3cb7f474ffd
benoit graziani <benoit.c.graziani@hotmail.fr>
2020-08-19 14:09:54 +0200

    new scss et new html Hairs et model ,controller de Hairs

--- app/controllers/hairs_controller.rb
+++ app/controllers/hairs_controller.rb
@@ -1,4 +1,6 @@
 class HairsController < ApplicationController
+  before_action :set_hair, only: [:show, :destroy]
+
   def index
     @hairs = policy_scope(Hair)
   end
@@ -10,6 +12,7 @@ class HairsController < ApplicationController
 
   def create
     @hair = Hair.new(hair_params)
+    @hair.user = current_user
     authorize @hair
     if @hair.save
       redirect_to hair_path(@hair)
@@ -19,19 +22,22 @@ class HairsController < ApplicationController
   end
 
   def show
-    @hair = Hair.find(params[:id])
+    @user = current_user
     authorize @hair
   end
 
   def destroy
-    @hair = Hair.find(params[:id])
     authorize @hair
     @hair.destroy
   end
 
   private
 
+  def set_hair
+    @hair = Hair.find(params[:id])
+  end
+
   def hair_params
-    params.require(:hair).permit(:title, :description, :color, :type, :quantity, :hair_length, :status, :price)
+    params.require(:hair).permit(:title, :description, :color, :type, :quantity, :hair_length, :status, :price, :nature)
   end
 end
