c85e54a1b0e62ac96d6c52827df70b753e085f39
Eugenie Avedikian <eugenieavedikian@gmail.com>
2020-08-19 12:27:32 +0200

    clean branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,8 +13,8 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
-  
-    def configure_permitted_parameters
+
+  def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :phone])
 
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
   end
 
   private
-  
+
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
