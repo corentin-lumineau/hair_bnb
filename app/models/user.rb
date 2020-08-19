553ab67edd69d11c14e4923d9f9f1110778d01bb
Corentin Lumineau <lumineau.corentin@gmail.com>
2020-08-19 16:57:41 +0200

    fix in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :hairs, dependent: :destroy
+  has_many :locations, dependent: :destroy
+
 end
