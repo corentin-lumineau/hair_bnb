4b85ccb222cbf77dcf26fca7f8a2762a8c50a15c
Corentin Lumineau <lumineau.corentin@gmail.com>
2020-08-20 16:58:33 +0200

    Nouvelle page de création de chevelure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,8 +27,8 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-
+import { input } from '../components/select_radio_buttons.js'
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+
+    input();
 });
