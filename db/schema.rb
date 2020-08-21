c02b8f67a5aaad1ef21750dbbe43d313450209ef
Corentin Lumineau <lumineau.corentin@gmail.com>
2020-08-21 12:40:42 +0200

    MAJ seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_091406) do
+ActiveRecord::Schema.define(version: 2020_08_21_094208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_091406) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "status", default: true
     t.string "nature"
+    t.text "soins"
     t.index ["user_id"], name: "index_hairs_on_user_id"
   end
 
