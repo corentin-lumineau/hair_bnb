2e8b1cbbf2ac2c6bd89e3791a1a730644a38bc9c
Corentin Lumineau <63396946+corentin-lumineau@users.noreply.github.com>
2021-10-06 17:15:45 +0200

    test

--- app/javascript/components/click_modal.js
+++ app/javascript/components/click_modal.js
@@ -3,15 +3,16 @@ const clickModal = () => {
   if (!loc_button) return
 
 
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    if (urlParams.get("retry") === "true") {
-      loc_button.click();
-      setTimeout(() => {
-        alert('laes dates ne peuvent etre choise')
-      }, 500)
-     }
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  if (urlParams.get("retry") === "true") {
+    loc_button.click();
+    setTimeout(() => {
+      alert('laes dates ne peuvent etre choise')
+    }, 500)
+  }
 }
+
 
 
 
